d6bc7a39c5ecbbd5ca91c51b254c7e34f9728395
younuspaperwala <52010552+younuspaperwala@users.noreply.github.com>
2022-07-10 16:24:08 -0500

    git pull and commit

--- WebApp0.0/AppBackend/urls.py
+++ WebApp0.0/AppBackend/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
 >>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
     path('FuelQuote.html', views.fuelQuote, name='fuelQuote'),
     path('FuelHistory.html', views.fuelHistory, name='fuelHistory'),
-    path('main.html', views.main, name='main'),
     path('login.html', views.login, name='login'),
     path('Signup.html', views.signup, name='signup'),
     path('ProfileManagement.html', views.ProfileManagement, name='ProfileManagement'),
 <<<<<<< HEAD
     path('confirmQuote',views.confirmQuote, name="confirmQuote")
 =======
+    path('logout',views.logout, name='logout')
 >>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
 ]
--- WebApp0.0/AppBackend/views.py
+++ WebApp0.0/AppBackend/views.py
@@ -63,16 +63,60 @@ def  fuelQuote(request):
     return render(request, 'FuelQuote.html',{'DeliveryAddress':'XYZ Address','Price':500,'Amount':5000})
 
 #Rendering login.html
+def  login(request):
+    #Insert code for login (login.html) here
+    if request.method == 'POST':
+        email = request.POST.get('email_address')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(username=email, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials invalid')
+            return redirect('login.html')
     #END CODE
+    else:
+        return render(request, 'login.html')
 
-    return render(request, 'main.html')
-
+#Logout Function
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
 #Rendering Signup.html
 def  signup(request):
     #Insert code for Sign up here
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email_address')
+        password = request.POST.get('password')
+        confirmemail = request.POST.get('confirm_email_address')
+        confirmpassword = request.POST.get('confirm_password')
 
+        if (email == confirmemail) and (confirmpassword == password):
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('Signup.html')
+            elif User.objects.filter(username = name).exists():
+                messages.info(request, 'Name Already Exists')
+                return redirect('Signup.html')
+            else:
+                user = User.objects.create_user(username = email, email = email, password = password)
+                user.save()
+                return redirect('login.html')
+        else:
+            if not(confirmpassword ==password):
+                messages.info(request, 'Passwords do not match')
+            else:
+                messages.info(request, 'Email Addresses do not match')
+            return redirect('signup')
+    else:
+        return render(request, 'Signup.html')  
+    
     #END CODE
-    return render(request, 'Signup.html')
+    
 
 #Rendering ProfileManagement.html
 def  ProfileManagement(request):
