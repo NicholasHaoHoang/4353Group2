291da2cd7e07d09a6c6d836f7c2fb27cb20c3c39
younuspaperwala <52010552+younuspaperwala@users.noreply.github.com>
2022-07-10 16:17:00 -0500

    FuelQuote and FuelHistory backend complete

--- WebApp0.0/AppBackend/urls.py
+++ WebApp0.0/AppBackend/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main, name='index'),
     path('FuelQuote.html', views.fuelQuote, name='fuelQuote'),
     path('FuelHistory.html', views.fuelHistory, name='fuelHistory'),
     path('main.html', views.main, name='main'),
     path('Signup.html', views.signup, name='signup'),
-    path('ProfileManagement.html', views.ProfileManagement, name='ProfileManagement')
+    path('ProfileManagement.html', views.ProfileManagement, name='ProfileManagement'),
+    path('confirmQuote',views.confirmQuote, name="confirmQuote")
 ]
--- WebApp0.0/AppBackend/views.py
+++ WebApp0.0/AppBackend/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import datetime
+import time
+from random import randint
 # Create your views here.
 
 #Rendering Index.html
@@ -14,10 +16,35 @@ def index(request):
 #Rendering FuelHistory.html
 def  fuelHistory(request):
     #Insert code for Fuel History Here
-
+    data = [
+        {
+        'Name': 'Younus',
+        'GallonsRequested': '100',
+        'DeliveryAddress': 'ABCD address',
+        'DeliveryDate': '10-20-2022',
+        'SuggestedPrice': '1000',
+        'AmountDue': '10'
+        },
+        {
+        'Name': 'Nick',
+        'GallonsRequested': '200',
+        'DeliveryAddress': 'XYZ address',
+        'DeliveryDate': '11-21-2022',
+        'SuggestedPrice': '200',
+        'AmountDue': '02'
+        },
+        {
+        'Name': 'Leena',
+        'GallonsRequested': '400',
+        'DeliveryAddress': 'DEF address',
+        'DeliveryDate': '11-21-2012',
+        'SuggestedPrice': '20',
+        'AmountDue': '50'
+        }
+    ]
     #END CODE
 
-    return render(request, 'FuelHistory.html')
+    return render(request, 'FuelHistory.html',{'data':data})
 
 #Rendering FuelQuote.html
 def  fuelQuote(request):
@@ -25,7 +52,7 @@ def  fuelQuote(request):
 
     #END CODE
 
-    return render(request, 'FuelQuote.html')
+    return render(request, 'FuelQuote.html',{'DeliveryAddress':'XYZ Address','Price':500,'Amount':5000})
 
 #Rendering main.html
 def  main(request):
@@ -48,3 +75,51 @@ def  ProfileManagement(request):
     
     #END CODE
     return render(request, 'ProfileManagement.html')
+
+def confirmQuote(request):
+    gallonsReq=request.GET['gallonsReq']
+    deliveryAddress=request.GET['deliveryAddress']
+    deliverydate=request.GET['deliverydate']
+    price=request.GET['price']
+    AmountDue=request.GET['AmountDue']
+    if int(request.GET['gallonsReq'])<1 or request.GET['gallonsReq'].isdigit()==False:
+        return render(request, "FuelQuote.html",{"gallonsReq":gallonsReq,"message":"Gallons requested must be a number"})
+    else:
+        gallonsReq=int(request.GET['gallonsReq'])
+        
+    if int(request.GET['price'])<1 or request.GET['price'].isdigit()==False:
+        return render(request, "FuelQuote.html",{"price":gallonsReq,"message":"Price must be a number"})
+    else:
+        price=int(request.GET['price'])
+    
+    if request.GET['AmountDue'].isdigit()==False:
+        return render(request, "FuelQuote.html",{"AmountDue":gallonsReq,"message":"Amount Due must be a number"})
+    else:
+        AmountDue=int(request.GET['AmountDue'])
+    if request.GET['AmountDue'].isdigit()==False:
+        return render(request, "FuelQuote.html",{"AmountDue":gallonsReq,"message":"Amount Due must be a number"})
+    else:
+        AmountDue=int(request.GET['AmountDue'])
+
+    month,day,year=deliverydate.split('-')
+    isValid=True
+    try:
+        datetime.datetime(int(month),int(day),int(year))
+
+    except ValueError:
+        isValid=False
+    if isValid==False:
+        return render(request, "FuelQuote.html",{"deliverydate":gallonsReq,"message":"Delivery date is not in correct format"})
+        
+    
+    print(gallonsReq)
+    print(deliveryAddress)
+    print(deliverydate)
+    print(price)
+    print(AmountDue)
+
+    rand = randint(0,1000)
+    timeNow = round(time.time()*1000)
+    quoteNo =rand+timeNow
+
+    return render(request, "confirmQuote.html",{'quoteNo':quoteNo})
