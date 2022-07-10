4ad7c5041598e85cf2eeaa5293a1ae53626527ed
younuspaperwala <52010552+younuspaperwala@users.noreply.github.com>
2022-07-10 16:27:23 -0500

    git commit and pull

--- WebApp0.0/AppBackend/urls.py
+++ WebApp0.0/AppBackend/urls.py
@@ -4,34 +4,14 @@ from . import views
 
 urlpatterns = [
 <<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     path('', views.main, name='index'),
-=======
     path('', views.index, name='index'),\
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
-=======
-    path('', views.index, name='index'),\
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
-=======
-    path('', views.index, name='index'),\
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
+
     path('FuelQuote.html', views.fuelQuote, name='fuelQuote'),
     path('FuelHistory.html', views.fuelHistory, name='fuelHistory'),
     path('login.html', views.login, name='login'),
     path('Signup.html', views.signup, name='signup'),
     path('ProfileManagement.html', views.ProfileManagement, name='ProfileManagement'),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('confirmQuote',views.confirmQuote, name="confirmQuote")
-=======
     path('logout',views.logout, name='logout')
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
-=======
-    path('logout',views.logout, name='logout')
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
-=======
-    path('logout',views.logout, name='logout')
->>>>>>> 20f4ae7308b8591b45fe1f349d6e126f42be15aa
-]
