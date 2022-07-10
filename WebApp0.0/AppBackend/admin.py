8fe067858b4fed7be7763854562810a4d059f034
younuspaperwala <52010552+younuspaperwala@users.noreply.github.com>
2022-07-10 16:17:57 -0500

    FuelQuote and FuelHistory Backend Complete

--- WebApp0.0/AppBackend/admin.py
+++ WebApp0.0/AppBackend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Feature, Profile
 
 # Register your models here.
+admin.site.register(Feature)
+admin.site.register(Profile)
--- WebApp0.0/AppBackend/models.py
+++ WebApp0.0/AppBackend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
+
+class Profile(models.Model):
+    name = models.CharField(max_length = 50)
+    address1 = models.CharField(max_length = 100)
+    address2 = models.CharField(max_length = 100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length = 2)
+    zipcode = models.CharField(max_length = 9)
