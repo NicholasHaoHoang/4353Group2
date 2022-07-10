7d6fdd3f327d8bbc36ce80121fb5c8e483392770
Nicholas Hoang <nhhoang3@CougarNet.UH.EDU>
2022-07-10 03:48:52 -0500

    renamed main.html to login.html

--- WebApp0.0/AppBackend/urls.py
+++ WebApp0.0/AppBackend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('FuelQuote.html', views.fuelQuote, name='fuelQuote'),
     path('FuelHistory.html', views.fuelHistory, name='fuelHistory'),
-    path('main.html', views.main, name='main'),
+    path('login.html', views.login, name='login'),
     path('Signup.html', views.signup, name='signup'),
     path('ProfileManagement.html', views.ProfileManagement, name='ProfileManagement')
 ]
--- WebApp0.0/AppBackend/views.py
+++ WebApp0.0/AppBackend/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.shortcuts import redirect
+from .models import Feature
 
 # Create your views here.
 
@@ -27,20 +31,43 @@ def  fuelQuote(request):
 
     return render(request, 'FuelQuote.html')
 
-#Rendering main.html
-def  main(request):
-    #Insert code for login (main.html) here
+#Rendering login.html
+def  login(request):
+    #Insert code for login (login.html) here
 
     #END CODE
 
-    return render(request, 'main.html')
+    return render(request, 'login.html')
 
 #Rendering Signup.html
 def  signup(request):
     #Insert code for Sign up here
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email_address')
+        password = request.POST.get('password')
+        confirmemail = request.POST.get('confirm_email_address')
+        confirmpassword = request.POST.get('confirm_password')
 
+        if (email == confirmemail) and (confirmpassword == password):
+            if User.object.filter(email=email).exists():
+                messages.info(request, 'Email Already Used')
+                return redirect('signup')
+            elif User.objects.filter(name = name).exists():
+                messages.info(request, 'Name Already Exists')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username = name, email = email, password = password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+    else:
+        return render(request, 'Signup.html')  
+    
     #END CODE
-    return render(request, 'Signup.html')
+    
 
 #Rendering ProfileManagement.html
 def  ProfileManagement(request):
