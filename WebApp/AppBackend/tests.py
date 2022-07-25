7acf08ea204f05ad2fd992482977a64d0d817d4b
Nicholas Hoang <nhhoang3@CougarNet.UH.EDU>
2022-07-24 19:33:10 -0500

    relocated templates directory

--- WebApp/AppBackend/tests.py
+++ WebApp/AppBackend/tests.py
@@ -2,6 +2,22 @@ from django.test import TestCase
 from .models import Profile, Feature
 
 # Create your tests here.
+class ModelTest(TestCase):
+    def create_profile(self):
+        prof = Profile()
+        prof.name = "Test A"
+        prof.email = "test@gmail.com"
+        prof.address1 = "0 test street"
+        prof.city = "TestCity"
+        prof.state = "TX"
+        prof.zipcode = "11111"
+
+        return prof
+    def test_profile_creation(self):
+        w = self.create_profile()
+        self.assertTrue(isinstance(w, Profile))
+
+    
 
 class URLTests(TestCase):
     def test_testIndex(self):
